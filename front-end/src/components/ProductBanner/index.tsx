eb9cf23633e68189043f283bbe5794d0ca01b2a5
Thiago Barcellos Mattos <th.mattos04@gmail.com>
2021-11-18 18:43:58 -0300

    init: init ProductBanner branch

---
+++ front-end/src/components/ProductBanner/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Image, Platform, Text, TouchableOpacity, View } from 'react-native';
+import { BannerDiv } from './styles';
+
+function ProductBanner() {
+
+    return (
+        <BannerDiv></BannerDiv>
+    )
+}
+
+export default ProductBanner;
---
+++ front-end/src/components/ProductBanner/styles.tsx
@@ -0,0 +1,9 @@
+import styled from "styled-components/native";
+
+export const BannerDiv = styled.View `
+    display: block;
+    top: 0;
+    width: 100%;
+    height: 20vh;
+    background-color: red;
+`;
