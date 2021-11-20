a96810a8570fc5a9263ca01d626b038151c0f9c7
Julia <jumartinino@gmail.com>
2021-11-20 13:59:09 -0300

    feature: criação da tela de login

---
+++ front-end/src/components/Cores/Colors.js
@@ -0,0 +1,12 @@
+const COLORS ={
+    darkGreen: '#58692E',
+    lightGreen: '#A0AA60',
+    darkBrown: '#39280D',
+    lightBrown: '#887E39',
+    white: '#F4F4F4',
+    black:'#000000'
+  
+  
+  }
+  
+export default COLORS;
