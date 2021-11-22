c2ed8e5430085763c309a3c4c84682438af60751
Julia <jumartinino@gmail.com>
2021-11-22 11:07:49 -0300

    feature: criação do container pedidos

--- front-end/src/components/Profile/index.tsx
+++ front-end/src/components/Profile/index.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
-import {View, Text, StyleSheet, SafeAreaView, StatusBar, Image, ScrollView,} from 'react-native';
+import {View, Text, TextInput, StyleSheet, SafeAreaView, StatusBar, Image, ScrollView, TouchableOpacity,ImageBackground} from 'react-native';
 import COLORS from '../Colors';
 
-import Icon from 'react-native-vector-icons/MaterialIcons';
+import Icon from 'react-native-vector-icons/Entypo';
 
 Icon.loadFont();
 
@@ -12,6 +12,7 @@ export default function Profile() {
   <SafeAreaView>
       <StatusBar barStyle = "dark-content" hidden = {false} backgroundColor = "white" translucent = {true}/>
       <ScrollView>
+
         <View style={styles.header}>
           <Text style={styles.title}>Olá, Rafael</Text>
           <Image
@@ -19,6 +20,51 @@ export default function Profile() {
             source={require('../../Images/rafael.jpg')}
           />
         </View>
+
+        <View style={styles.editContainer}>
+
+          <View style={styles.editHeader}>
+          <Text style={styles.containersTitle}>Meus dados:</Text>
+          <TouchableOpacity>
+          <Icon name='pencil' style={styles.icon} />
+          </TouchableOpacity>
+          </View>
+
+          <View style={styles.dados}>
+            <Text>Nome: Pedro Alves Faria</Text>
+            <Text>Cpf: 123.456.789-10</Text>
+            <Text>Email: Rafael@email.com</Text>
+            <Text>Endereço: Rua das pedras 123, bairro alto</Text>
+          </View>
+        </View>
+
+        <View style={styles.pedidosFavoritosContainer}>
+          <Text style={styles.containersTitle}>Meus pedidos:</Text>
+
+          <ScrollView horizontal={true} showsHorizontalScrollIndicator ={false}>
+            <TouchableOpacity style={styles.pedidosEach}>
+              <Image source={require('../../Images/boardprod.png')}/>
+            </TouchableOpacity>
+
+            <TouchableOpacity style={styles.pedidosEach}>
+              <Image source={require('../../Images/boardprod.png')}/>
+            </TouchableOpacity>
+
+            <TouchableOpacity style={styles.pedidosEach}>
+              <Image source={require('../../Images/boardprod.png')}/>
+            </TouchableOpacity>
+            
+            <TouchableOpacity style={styles.pedidosEach}>
+              <Image source={require('../../Images/boardprod.png')}/>
+            </TouchableOpacity>
+          </ScrollView>
+        </View>
+
+        <View style={styles.pedidosFavoritosContainer}>
+          <Text style={styles.containersTitle}>Meus favoritos:</Text>
+
+        </View>
+        
       </ScrollView>
   </SafeAreaView>
   )
@@ -28,18 +74,54 @@ const styles= StyleSheet.create({
   header:{
     flexDirection: 'row',
     justifyContent: 'space-between',
-    paddingVertical: 10,
-    paddingHorizontal:15,
+    paddingVertical: 15,
+    paddingHorizontal:25,
     marginTop:30,
   },
   title:{
     marginTop:18,
     fontSize:20,
-    marginLeft:20
   },
   profile:{
     height:70,
     width:70,
-    borderRadius:35,  
+    borderRadius:35,
+  },
+  editContainer:{
+    paddingVertical: 15,
+    paddingHorizontal:10,
+    backgroundColor:COLORS.lightGreen,
+    width:'90%',
+    alignSelf:'center',
+    borderRadius:10,
+  },
+  editHeader:{
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    },
+  containersTitle:{
+    fontSize:20,
+  },
+  icon:{
+    color:COLORS.darkBrown,
+    fontSize:30,
+  },
+  dados:{
+
+  },
+  pedidosFavoritosContainer:{
+    paddingVertical: 15,
+    paddingHorizontal:10,
+    width:'90%',
+    alignSelf:'center',
+    borderRadius:10,
+    borderColor:COLORS.darkGreen,
+    borderWidth:2,
+    marginTop:20
+  },
+  pedidosEach:{
+    marginTop:10,
+    backgroundColor:COLORS.darkBrown,
+    width:'100%',
   },
 })
