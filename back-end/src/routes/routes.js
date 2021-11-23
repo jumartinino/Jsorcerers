daf1debd74721100d8d3d948b839253b959c652c
Carlos Eduardo Valladares <carloseduardovdm@gmail.com>
2021-11-23 19:48:12 -0300

    refactor: incluindo Validador e atualizando as rotas de create

--- back-end/src/routes/routes.js
+++ back-end/src/routes/routes.js
@@ -10,58 +10,59 @@ const User_ShopController = require('../controllers/User_ShopController');
 const UserController = require('../controllers/UserController');
 const router = Router();
 const adminMiddleware = require("../middlewares/admin");
+const validator = require("../config/validator");
 
 router.get('/buys',BuyingController.index);
 router.get('/buys/:id',BuyingController.show);
-router.post('/buys',BuyingController.create);
+router.post('/buys', validator.validationBuying, BuyingController.create);
 router.put('/buys/:id', BuyingController.update);
 router.delete('/buys/:id', BuyingController.destroy);
 
 router.get('/carts',CartController.index);
 router.get('/carts/:id',CartController.show);
-router.post('/carts',CartController.create);
+router.post('/carts', validator.validationCart, CartController.create);
 router.put('/carts/:id', CartController.update);
 router.delete('/carts/:id', CartController.destroy);
 
 router.get('/comments',CommentController.index);
 router.get('/comments/:id',CommentController.show);
-router.post('/comments',CommentController.create);
+router.post('/comments', validator.validationComment, CommentController.create);
 router.put('/comments/:id', CommentController.update);
 router.delete('/comments/:id', CommentController.destroy);
 
 router.get('/favorites_list',FavoritesController.index);
 router.get('/favorites_list/:id',FavoritesController.show);
-router.post('/favorites_list',FavoritesController.create);
+router.post('/favorites_list', FavoritesController.create);
 router.put('/favorites_list/:id', FavoritesController.update);
 router.delete('/favorites_list/:id', FavoritesController.destroy);
 
 router.get('/posts',PostController.index);
 router.get('/posts/:id',PostController.show);
-router.post('/posts',PostController.create);
+router.post('/posts', validator.validationPost, PostController.create);
 router.put('/posts/:id', PostController.update);
 router.delete('/posts/:id', adminMiddleware, PostController.destroy);
 
 router.get('/products',ProductController.index);
 router.get('/products/:id',ProductController.show);
-router.post('/products',ProductController.create);
+router.post('/products', validator.validationProduct, ProductController.create);
 router.put('/products/:id', ProductController.update);
 router.delete('/products/:id', ProductController.destroy);
 
 router.get('/questions',QuestionController.index);
 router.get('/questions/:id',QuestionController.show);
-router.post('/questions',QuestionController.create);
+router.post('/questions', validator.validationQuestion, QuestionController.create);
 router.put('/questions/:id', QuestionController.update);
 router.delete('/questions/:id', QuestionController.destroy);
 
 router.get('/shops',User_ShopController.index);
 router.get('/shops/:id',User_ShopController.show);
-router.post('/shops',User_ShopController.create);
+router.post('/shops', validator.validationUser_Shop, User_ShopController.create);
 router.put('/shops/:id', User_ShopController.update);
 router.delete('/shops/:id', User_ShopController.destroy);
 
 router.get('/users',UserController.index);
 router.get('/users/:id',UserController.show);
-router.post('/users',UserController.create);
+router.post('/users', validator.validationUser, UserController.create);
 router.put('/users/:id', UserController.update);
 router.delete('/users/:id', adminMiddleware, UserController.destroy);
 
