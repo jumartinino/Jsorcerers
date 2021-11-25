fd713d629f4aca4fb8b2cce8d7aa55fdb165317c
Thiago Barcellos Mattos <th.mattos04@gmail.com>
2021-11-25 02:49:06 -0300

    refactor: card de produtos

--- front-end/App.tsx
+++ front-end/App.tsx
@@ -1,29 +1,19 @@
-import React = require('react');
+// import React = require('react');
+import React from 'react';
 import { StyleSheet, Text, View, StatusBar } from 'react-native';
 import {NavigationContainer} from '@react-navigation/native'
-import HomeScreen from './src/components/screens/HomeScreen';
+import HomeScreen from './src/screens/HomeScreen';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
 const {Navigator, Screen} = createNativeStackNavigator();
 
 const App = () => {  
   return ( 
-    <NavigationContainer style={styles.container}>
+    <NavigationContainer>
       <Navigator>
             <Screen options={{headerShown:false}} name="Home" component={HomeScreen}></Screen>
       </Navigator>    
     </NavigationContainer>  
   );
 };
-
-const styles = StyleSheet.create({
-  container: {
-    width: "100%",
-    flex: 1,
-    backgroundColor: 'black',
-    marginLeft: 20, 
-    marginRight: 20,
-    justifyContent: 'center',
-  },
-});
 
 export default App;
